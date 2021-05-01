ea0b4810c27e6081a16a389858bc6498916f1f74
Pascal Hubrecht <hansemschnokeloch@users.noreply.github.com>
2021-05-01 21:39:21 +0200

    Add some color

--- bookmarks.sh
+++ bookmarks.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# colors
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LGRAY='\033[0;37m'
+DGRAY='\033[1;30m'
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LBLUE='\033[1;34m'
+LPURPLE='\033[1;35m'
+LCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
 
 CONF=$HOME/.bookmarks.conf
 VI=nvim
@@ -20,7 +38,7 @@ echo
 
 for k in "${KEYS[@]}"
 do
-    echo "[ $k ] : ${BOOKMARKS[$k]}"
+    echo -e "[ $YELLOW$k$NC ] : $LGREEN${BOOKMARKS[$k]}$NC"
 done
 
 echo
